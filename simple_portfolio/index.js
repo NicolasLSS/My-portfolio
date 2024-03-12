3af7dc7713f29bb34a0d30d1ef20519df4a33338
Nícolas Silva <163082588+NicolasLSS@users.noreply.github.com>
2024-03-12 19:35:42 -0300

    Add files via upload

---
+++ simple_portfolio/index.js
@@ -0,0 +1,40 @@
+const Letters="ABCDEFGHIJKLMNOPQRSTUVHXYZ#@!/[]123456789";
+
+let AnimInterval=0;
+
+if(document != null && AnimInterval != null){
+
+	AnimStart();
+}
+else{
+	
+}
+function AnimStart (argument) {
+
+	document.querySelector("h2").onmouseover = event =>{
+
+	if(AnimInterval==0){
+		AnimInterval+=1;
+
+
+		let iterations = 0;
+
+	const interval = setInterval(() => {
+		event.target.innerText = event.target.innerText.split("").map((letter,index) => {
+			if(index < iterations){
+				return event.target.dataset.value[index];
+			}
+			return Letters[Math.floor(Math.random() * 26)]
+		}).join("");
+
+		if(iterations>=event.target.dataset.value.length){
+			clearInterval(interval);
+			AnimInterval-=1;
+		}
+		iterations += 1/2;
+	},30);
+
+
+	}
+}
+}
